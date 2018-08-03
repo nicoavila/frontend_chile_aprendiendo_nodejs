2807a54b434c627cfd35c5f3ba86860e884300a3
Nicolas Avila <nicolasavila.a@gmail.com>
2018-08-03 00:23:46 -0400

    feat: integracion con api de transantiago

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 //Configuración de rutas
 app.use('/', indexRouter);
 app.use('/usuarios', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
 /* Página de inicio */
 router.get('/', (req, res, next) => {
   return res.status(200).json({ message: 'CASTOR API v1.0' });
+});
+
+/* Obtiene toda la lista de recorridos desde el Transantiago */
+router.get('/transantiago', (req, res, next) => {
+  let opciones = {
+    method: 'GET',
+    url: 'http://www.transantiago.cl/restservice/rest/getservicios/all',
+    json: true
+  };
+  request(opciones, (err, resp, body) => {
+    if (err) {
+      reject(err);
+    }
+    return res.status(200).json(body);
+  });
 });
 
 module.exports = router;
